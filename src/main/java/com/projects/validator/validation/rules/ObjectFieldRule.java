d848c42033ef43f703235bf8e1b5fec160b6cdbe
yishu <yishu.ding2019@googlemail.com>
2020-10-28 16:18:10 +0000

    Added missing class change

--- src/main/java/com/projects/validator/validation/rules/ObjectFieldRule.java
+++ src/main/java/com/projects/validator/validation/rules/ObjectFieldRule.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 import com.projects.validator.validation.Rule;
 
-public class ObjectFieldRule extends Rule{
+public class ObjectFieldRule implements Rule{
 	
-	
-	List<Rule> rulesList = new ArrayList<Rule>();
+	public String fieldName;
+	public List<Rule> rulesList = new ArrayList<Rule>();
 	
 	public ObjectFieldRule() {
 		
@@ -21,41 +21,63 @@ public class ObjectFieldRule extends Rule{
 	
 	
 	
-	public ObjectFieldRule(List<Rule> _rulesList) {
+	public ObjectFieldRule(String _fieldName, List<Rule> _rulesList) {
 		
 		rulesList = _rulesList;
 	}
 
 	@Override
 	public boolean validate(Object actualValue) {
-		Object fieldValue = null;
-		
-		
-		
-		for(Rule rule: rulesList) {	
-			//isInstance check left object is child of right side class, 
-			//while isAssignableFrom check left is parent of right side class
-			//if(rule.getClass().getSuperclass().isAssignableFrom(PrimitiveFieldRule.class)) 
-			if(rule instanceof PrimitiveFieldRule ) {
-				fieldValue = actualValue;
-					
-			}
-			else {
-				try {
-					Field objectField = actualValue.getClass().getField(rule.fieldName);
-					fieldValue = objectField.get(actualValue);
-					
-					
-				} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
-				}
+		if(fieldName!=null) {
+			Object fieldValue = null;
+			
+			try {
+				Field objectField = actualValue.getClass().getField(fieldName);
+				fieldValue = objectField.get(actualValue);
 				
 				
-			}
-			if(!rule.validate(fieldValue))
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				e.printStackTrace();
 				return false;
-					
+			} catch(Exception e) {
+				return false;
+			}
+			
+			
+			for(Rule rule: rulesList) {	
+				if(!rule.validate(fieldValue))
+					return false;
+			}
+		
+			
 		}
+		
+		
+		/*	
+			for(Rule rule: rulesList) {	
+				//isInstance check left object is child of right side class, 
+				//while isAssignableFrom check left is parent of right side class
+				//if(rule.getClass().getSuperclass().isAssignableFrom(PrimitiveFieldRule.class)) 
+				if(rule instanceof PrimitiveFieldRule ) {
+					fieldValue = actualValue;
+						
+				}
+				else {
+					try {
+						Field objectField = actualValue.getClass().getField(rule.fieldName);
+						fieldValue = objectField.get(actualValue);
+						
+						
+					} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+						e.printStackTrace();
+					}
+					
+					
+				}
+				if(!rule.validate(fieldValue))
+					return false;
+						
+			}*/
 		
 		return true;
 	}
